134d688591bfffb0a8468c00e925fd004d3afed8
AndiMSF <andimuhammadsatriafadhil77@gmail.com>
2024-06-04 10:11:09 +0700

    api get all absen by nik

--- src/main/java/com/treemaswebapi/treemaswebapi/controller/DetailData/Absen/AbsenControllerWeb.java
+++ src/main/java/com/treemaswebapi/treemaswebapi/controller/DetailData/Absen/AbsenControllerWeb.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class AbsenControllerWeb {
     @GetMapping("/absen-view")
     public ResponseEntity<Map<String, Object>> absenGet() {
         ResponseEntity<Map<String, Object>> response = service.absenGet();
+        return response;
+    }
+
+    @GetMapping("/absen-view-sendiri")
+    public ResponseEntity<Map<String, Object>> absenGetSendiri(@RequestHeader("Authorization") String jwtToken) {
+        ResponseEntity<Map<String, Object>> response = service.absenGetSendiri(jwtToken);
         return response;
     }
 }
--- src/main/java/com/treemaswebapi/treemaswebapi/service/DetailData/Absen/AbsenServiceWeb.java
+++ src/main/java/com/treemaswebapi/treemaswebapi/service/DetailData/Absen/AbsenServiceWeb.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
 
+import com.treemaswebapi.treemaswebapi.config.JwtService;
 import com.treemaswebapi.treemaswebapi.entity.AbsenEntity.AbsenEntity;
 import com.treemaswebapi.treemaswebapi.entity.JabatanEntity.JabatanEntity;
 import com.treemaswebapi.treemaswebapi.entity.SysUserEntity.SysUserEntity;
@@ -24,10 +26,62 @@ import lombok.RequiredArgsConstructor;
 public class AbsenServiceWeb {
     private final AbsenRepository absenRepository;
     private final SysUserRepository sysUserRepository;
+    private final JwtService jwtService;
     
     public ResponseEntity<Map<String, Object>> absenGet() {
         try {
             List<AbsenEntity> absenList  = absenRepository.findAll();
+            List<Map<String, Object>> responseData = new ArrayList<>();
+
+            for (AbsenEntity absen : absenList) {
+                Map<String, Object> absenData = new HashMap<>();
+                absenData.put("nik", absen.getNik());
+                absenData.put("namaKaryawan", absen.getNama());
+                absenData.put("tanggal", absen.getTglAbsen());
+                absenData.put("lokasiMasuk", absen.getLokasiMsk());
+                absenData.put("jamMasuk", absen.getJamMsk());
+                absenData.put("lokasiPulang", absen.getLokasiPlg());
+                absenData.put("jamPulang", absen.getJamPlg());
+                absenData.put("catatanTerlambat", absen.getNoteTelatMsk());
+                absenData.put("totalJamKerja", absen.getTotalJamKerja());
+                absenData.put("isCuti", absen.getIsCuti());
+                absenData.put("isOther", absen.getIsOther());
+                absenData.put("isSakit", absen.getIsSakit());
+                absenData.put("isWfh", absen.getIsWfh());
+                absenData.put("isLembur", absen.getIsLembur());
+                absenData.put("projectId", absen.getProjectId());
+                // Cari role dari setiap nik di table sys_user
+                Optional<SysUserEntity> sysUserOptional = sysUserRepository.findByUserId(absen.getNik());
+                if (sysUserOptional.isPresent()) {
+                    SysUserEntity sysUser = sysUserOptional.get();
+                    absenData.put("role", sysUser.getRole());
+                }
+
+                    responseData.add(absenData);
+                }
+        
+            Map<String, Object> response = new HashMap<>();
+            response.put("status", "Success");
+            response.put("message", "Retrieved");
+            response.put("data", responseData);
+        
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("status", "Failed");
+            response.put("message", "Failed to retrieve absen");
+            response.put("error", e.getMessage());
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    public ResponseEntity<Map<String, Object>> absenGetSendiri(@RequestHeader("Authorization") String jwtToken) {
+        try {
+            // Cari siapa yang akses api ini
+            String token = jwtToken.substring(7);
+            String userToken = jwtService.extractUsername(token);
+            List<AbsenEntity> absenList  = absenRepository.findAllByNik(userToken);
             List<Map<String, Object>> responseData = new ArrayList<>();
 
             for (AbsenEntity absen : absenList) {
