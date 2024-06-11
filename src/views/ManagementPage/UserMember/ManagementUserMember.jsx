4f93f582bada6fcc4d34c458e957cf602ed8a99a
AndiMSF <andimuhammadsatriafadhil77@gmail.com>
2024-06-11 09:10:17 +0700

    masih progress di management user member

--- src/views/ManagementPage/UserMember/ManagementUserMember.jsx
+++ src/views/ManagementPage/UserMember/ManagementUserMember.jsx
@@ -9,7 +9,7 @@ import "react-data-table-component-extensions/dist/index.css";
 import DropdownMenu from "../../../components/Elements/DropdownMenu/DropdownMenu";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom/dist";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, Form } from "react-bootstrap";
 import Swal from "sweetalert2";
 import axios from "axios";
 // Switch
@@ -29,7 +29,7 @@ const ManagementUserMember = () => {
   const [dropdownItems, setDropdownItems] = useState("Pilih User");
   const [isAll, setIsAll] = useState(false);
   const [nikLeader, setNikLeader] = useState(null);
-  const [checkedState, setCheckedState] = useState({});
+  const [checkedState, setCheckedState] = useState([{}]);
   // Switch
   const [checked, setChecked] = useState(true);
 
@@ -49,86 +49,86 @@ const ManagementUserMember = () => {
 
     // If the switch is turned off (unchecked)
     if (!isChecked) {
-        try {
-            if (token) {
-                const response = await axios.delete(
-                    `http://localhost:8081/api/management/user-member-view/delete?nikLeader=${nikLeader}&nikUser=${userId}`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: "Bearer " + token,
-                        },
-                    }
-                );
-
-                if (response.data.status === "Success") {
-                    Swal.fire({
-                        title: "Success!",
-                        text: response.data.message + " For Nik " + userId,
-                        icon: "success",
-                    });
-                } else {
-                    Swal.fire({
-                        title: "Error!",
-                        text: response.data.message,
-                        icon: "error",
-                    });
-                }
-            } else {
-                navigate("/");
+      try {
+        if (token) {
+          const response = await axios.delete(
+            `http://localhost:8081/api/management/user-member-view/delete?nikLeader=${nikLeader}&nikUser=${userId}`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+              },
             }
-        } catch (error) {
+          );
+
+          if (response.data.status === "Success") {
             Swal.fire({
-                title: "Error!",
-                text: error.response.data.message,
-                icon: "error",
+              title: "Success!",
+              text: response.data.message + " For Nik " + userId,
+              icon: "success",
             });
+          } else {
+            Swal.fire({
+              title: "Error!",
+              text: response.data.message,
+              icon: "error",
+            });
+          }
+        } else {
+          navigate("/");
         }
+      } catch (error) {
+        Swal.fire({
+          title: "Error!",
+          text: error.response.data.message,
+          icon: "error",
+        });
+      }
     } else {
-        // If the switch is turned on (checked)
-        const requestData = {
-            nikLeader: nikLeader,
-            nikUser: userId,
-        };
+      // If the switch is turned on (checked)
+      const requestData = {
+        nikLeader: nikLeader,
+        nikUser: userId,
+      };
 
-        try {
-            if (token) {
-                const response = await axios.post(
-                    "http://localhost:8081/api/management/user-member-view/add",
-                    requestData,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: "Bearer " + token,
-                        },
-                    }
-                );
-
-                if (response.data.status === "Success") {
-                    Swal.fire({
-                        title: "Success!",
-                        text: response.data.message + " For Nik " + userId,
-                        icon: "success",
-                    });
-                } else {
-                    Swal.fire({
-                        title: "Error!",
-                        text: response.data.message,
-                        icon: "error",
-                    });
-                }
-            } else {
-                navigate("/");
+      try {
+        if (token) {
+          const response = await axios.post(
+            "http://localhost:8081/api/management/user-member-view/add",
+            requestData,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+              },
             }
-        } catch (error) {
+          );
+
+          if (response.data.status === "Success") {
             Swal.fire({
-                title: "Error!",
-                text: error.response.data.message,
-                icon: "error",
+              title: "Success!",
+              text: response.data.message + " For Nik " + userId,
+              icon: "success",
             });
+          } else {
+            Swal.fire({
+              title: "Error!",
+              text: response.data.message,
+              icon: "error",
+            });
+          }
+        } else {
+          navigate("/");
         }
+      } catch (error) {
+        Swal.fire({
+          title: "Error!",
+          text: error.response.data.message,
+          icon: "error",
+        });
+      }
     }
-};
+  };
 
   const Android12Switch = styled(Switch)(({ theme }) => ({
     padding: 8,
@@ -279,6 +279,81 @@ const ManagementUserMember = () => {
     }
   }, [navigate]);
 
+  // Handle Switch Aktif
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8081/api/management/user-member-active",
+          {
+            method: "GET", // Sesuaikan metode sesuai kebutuhan (GET, POST, dll.)
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`, // Sertakan token di sini
+            },
+          }
+        );
+        // Tambahkan penanganan kesalahan di sini
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.status === "Success") {
+          const initialSwitchStates = {};
+          data.data.forEach((items) => {
+            console.log("INI ITEMS " + JSON.stringify(items, null, 2));
+            console.log("NIK LEADER " + nikLeader);
+            console.log("ITEMS NIK LEADER " + items.nikLeader);
+
+            if (nikLeader === items.nikLeader) {
+              console.log("MASUK");
+              initialSwitchStates[items.nikUser] = true
+            } else {
+              initialSwitchStates[items.nikUser] = false;
+            }
+
+            console.log(
+              "CHECKED STATE " + JSON.stringify(initialSwitchStates, null, 2)
+            );
+          });
+          setCheckedState(initialSwitchStates);
+        } else {
+          setError("Failed to fetch data");
+        }
+      } catch (error) {
+        if (error.message.includes("HTTP error!")) {
+          const statusCode = parseInt(error.message.split(" ").pop());
+          console.log("Status Code:", statusCode);
+
+          if (statusCode === 401) {
+            console.log("Masuk 401");
+            // Token expired, remove token from local storage and redirect to login
+            localStorage.removeItem("authToken");
+          } else {
+            console.log("Gak Masuk 401");
+            setError(`Error fetching data: ${error.message}`);
+          }
+        } else {
+          console.log("Gak Masuk 401");
+          setError(`Error fetching data: ${error.message}`);
+        }
+      }
+    };
+
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      setIstoken(token);
+      fetchData(); // Panggil fungsi fetchData setelah mendapatkan token
+      console.log("Token: " + token);
+    } else {
+      navigate("/login");
+    }
+  }, [navigate, nikLeader]);
+
   const handleDropdownSelect = (selectedUser) => {
     const selectedItem = dropdownData.find(
       (user) => user.fullName === selectedUser
@@ -294,28 +369,23 @@ const ManagementUserMember = () => {
     console.log("Kepencet");
   };
 
+  console.log(checkedState);
+
   const columns = [
     {
-      name: (
-          <FormControlLabel
-              control={<Android12Switch />}
-              onChange={handleAllChange}
-              inputProps={{ "aria-label": "controlled" }}
-              style={{ marginLeft: 0 }}
-          />
-      ),
+      name: "Select",
       sortable: false,
       cell: (d) => (
-          <FormControlLabel
-              key={d.userId}
-              control={<Android12Switch />}
-              onChange={(event) => handleChange(event, d.userId)}
-              checked={checkedState[d.userId] || false}
-              inputProps={{ "aria-label": "controlled" }}
-              style={{ marginLeft: 0 }}
-          />
+        <Form.Check
+          type="switch"
+          id={`custom-switch-${d.projectId}`}
+          onChange={(event) => handleChange(event, d.userId)}
+          checked={checkedState[d.userId] || false}
+          inputProps={{ "aria-label": "controlled" }}
+          style={{ marginLeft: 0 }}
+        />
       ),
-  },
+    },
     {
       name: "NIK",
       selector: (row) => row.userId,
